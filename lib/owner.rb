e38b72a704444e72a0ee34e18f1445af648fc548
Claire Harmer <claire.f.harmer@gmail.com>
2018-08-30 19:30:40 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,5 @@
+
+require "pry"
 class Owner
   attr_accessor :name, :pets
   attr_reader :species
@@ -60,5 +62,6 @@ class Owner
 
   def list_pets
     "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+    binding.pry
   end
 end
